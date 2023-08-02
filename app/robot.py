1a9c72e8533fd24003c1c7a273c84e6edee3d24e
Antonio Vrdoljak <tonivrdoljak165@gmail.com>
2023-08-03 00:57:21 +0200

    Added functionality for setting the position

--- app/robot.py
+++ app/robot.py
@@ -6,21 +6,36 @@ class RobotSimulator:
         self.valid_directions = ['NORTH', 'EAST', 'SOUTH', 'WEST']
         self.placed = False
 
+    def place(self, x, y, direction):
+        if direction.upper() in self.valid_directions:
+            self.x = x
+            self.y = y
+            self.direction = direction.upper()
+            self.placed = True
+
 
 def main():
+    command_place = 'PLACE'
     robot = RobotSimulator()
 
     while True:
         commands = input("Enter commands (PLACE X,Y,F MOVE LEFT RIGHT REPORT or EXIT ").strip().upper().split()
 
         if not commands:
-            print("Incorrect input, please try again")
+            print("Incorrect input, please try again.")
             continue
 
         for command in commands:
             if command == 'EXIT':
-                print("Exiting the toy robot simulator.")
+                print("Exiting the robot simulator.")
                 return
+            elif command.startswith(command_place):
+                place_index = commands.index(command)
+                additional_place_command = commands[place_index + 1]
+                command = command + ' ' + additional_place_command
+                _, args = command.split()
+                x, y, direction = args.split(',')
+                robot.place(int(x), int(y), direction)
 
 
 if __name__ == "__main__":
