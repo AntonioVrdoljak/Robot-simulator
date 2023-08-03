977228362ef1307941c6a89d3235ab3880eaa19a
Antonio Vrdoljak <tonivrdoljak165@gmail.com>
2023-08-03 19:51:54 +0200

    Added robot test for ignore all commands before command PLACE

--- tests/test_robot.py
+++ tests/test_robot.py
@@ -47,6 +47,29 @@ class TestRobot(unittest.TestCase):
             robot.report()
         self.assertIn("INFO:root:Output: 2,2,NORTH", log.output[0])
 
+    def test_ignore_commands_before_place(self):
+        robot = RobotSimulator()
+        commands = ['MOVE', 'LEFT', 'RIGHT', 'REPORT', 'PLACE 2,2,NORTH', 'REPORT']
+
+        for command in commands:
+            if command.startswith('PLACE'):
+                _, args = command.split(' ', 1)
+                x, y, direction = args.split(',')
+                robot.place(int(x), int(y), direction)
+            elif robot.placed:
+                if command == 'MOVE':
+                    robot.move()
+                elif command == 'LEFT':
+                    robot.left()
+                elif command == 'RIGHT':
+                    robot.right()
+                elif command == 'REPORT':
+                    with self.assertLogs() as log:
+                        robot.report()
+                    self.assertIn("INFO:root:Output: 2,2,NORTH", log.output[0])
+            else:
+                self.assertFalse(robot.x is not None and robot.y is not None and robot.direction is not None)
+
 
 if __name__ == '__main__':
     unittest.main()
