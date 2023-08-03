b3707b780126580410cdb78d703433a81f8ffd32
Antonio Vrdoljak <tonivrdoljak165@gmail.com>
2023-08-03 19:22:28 +0200

    Added test for invalid place command

--- tests/test_robot.py
+++ tests/test_robot.py
@@ -10,6 +10,14 @@ class TestRobot(unittest.TestCase):
         self.assertEqual(robot.y, 3)
         self.assertEqual(robot.direction, 'NORTH')
 
+    def test_invalid_place_command(self):
+        robot = RobotSimulator()
+        robot.place(5, 2, 'NORTH')  # Invalid position
+        self.assertFalse(robot.x is not None and robot.y is not None and robot.direction is not None)
+
+        robot.place(2, 3, 'INVALID')  # Invalid direction
+        self.assertFalse(robot.x is not None and robot.y is not None and robot.direction is not None)
+
 
 if __name__ == '__main__':
     unittest.main()
